41adf15b536c0d7c18e58fbe3491e0a5381d2d1b
Felipe Farias <fefarias97@gmail.com>
2021-09-02 15:32:48 -0300

    resolução exercício gritador

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frasedigitada;
+            Console.WriteLine("Digite uma frase: ");
+            frasedigitada = Console.ReadLine();
+
+            Console.WriteLine(frasedigitada.ToUpper());
+
+        }
+    }
+}
